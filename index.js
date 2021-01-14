53de25e95d1f8174ff42f26cdc6e79b0782c6f2b
maro <kamanbe0120@gmail.com>
2021-01-14 00:25:12 +0000

    Add translation api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { Translate } = require('@google-cloud/translate').v2;
+const translateClient = new Translate();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,4 +13,12 @@ app.listen(port, () => {
 app.get("/hello", async (req, res) => {
   const word = req.query.w;
   res.send(`Hello, you entered ${word}!`);
+});
+
+app.get('/translate/:lang', async (req, res) => {
+  const result = await translateClient.translate(
+    'こんにちは',
+    req.params.lang
+  );
+  res.send(`${result[0]}\n\n`);
 });
---
+++ start.sh
@@ -0,0 +1,2 @@
+export GOOGLE_APPLICATION_CREDENTIALS=../etc/gcr_project/api-project-286902-f89cc3a68d39.json
+npm run start
